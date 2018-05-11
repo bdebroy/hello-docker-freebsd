bee0f9fe0b020b24fcb70ec83fbee1368fa0f975
Brandon Debroy <brandon.debroy@xdevgt.com>
2018-05-11 01:30:37 -0600

    more lines

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-portsnap auto && \
-pkg install -y portdowngrade && \
-portdowngrade lang/go r441745  && \
+#portsnap auto && \
+echo "vfs.zfs.prefetch_disable=0" >> /boot/loader.conf && \
+pkg install -y ca_root_nss portdowngrade && \
+cd /usr/ports/ && portdowngrade lang/go r441745  && \
 rm -r /usr/ports/lang/go/ && \
 mv /usr/ports/go /usr/ports/lang/go && \
-pkg install -y ca_root_nss bash gettext-runtime sqlite3 && \
-    cd /usr/ports/sysutils/docker-freebsd/ && make install clean BATCH=yes && \
-    dd if=/dev/zero of=/usr/local/dockerfs bs=1024K count=4000 && \
+cd /usr/ports/lang/go && make install clean BATCH=yes && \
+pkg lock -y go
+pkg install -y docker-freebsd && \
+    dd if=/dev/zero of=/usr/local/dockerfs bs=1024K count=3000 && \
     zpool create -f zroot /usr/local/dockerfs && \
     zfs create -o mountpoint=/usr/docker zroot/docker && \
     sysrc -f /etc/rc.conf docker_enable="YES" && \
     pw groupadd docker && \
     pw groupmod docker -M $USER && \
+    pw groupmod operator -m $USER && \
     service docker start
